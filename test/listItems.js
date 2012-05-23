15999e04d17f8839bcdd6ec28bbcbb871da1a1b1
pgschk <github@pgmail.net>
2012-05-23 22:05:58 +0200

    Started 0.2.0 development; started switching to API version 2; supporting SSL requests now

--- test/listItems.js
+++ test/listItems.js
@@ -1,7 +1,7 @@
 
 
-var client = require('../lib/index.js').pastyclient('localhost', 8888);
-var token = 'ee9ed55231cafe69660bc58717d9fb26b7da7fc8faab847cace3ff381e957e792d4d44fe5103262811bd8f0914666daa9a2325c45d3f9c436d74661c5ca2c636';
+var client = require('../lib/index.js').pastyclient('mario.blafaselblub.net', 8888);
+var token = '';
 
 client.listItems(token, function(E,items) {
   if(E === null) {
